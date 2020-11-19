4e0def47bf6d85d11d11cc6d19df0054a9dba6e0
Gary Richards <gary.richards@acuris.com>
2020-11-19 14:25:48 +0000

    Fixes for dockerhub pull limits
    Add a way to specify login credentials for any container registry that
    works with docker.
    
    Try to work out the registry that applies to the supplied image and lookup
    environment vars specific to that registry for a username and password.
    
    Ie. for docker hub (index.docker.io) you would provide environment vars
    CDFLOW2_DOCKER_AUTH_INDEX_DOCKER_IO_USERNAME
    CDFLOW2_DOCKER_AUTH_INDEX_DOCKER_IO_PASSWORD
    
    For a private registry (my.private.reg:5000) you would provide env vars
    CDFLOW2_DOCKER_AUTH_MY_PRIVATE_REG_5000_USERNAME
    CDFLOW2_DOCKER_AUTH_MY_PRIVATE_REG_5000_PASSWORD
    
    The CDFLOW2_DOCKER_AUTH_ prefix provides a nice simple way for the config
    container to know which environment variables to pass through to the release
    container

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,14 +1,18 @@
 package app
 
 import (
+	"bufio"
 	"encoding/base64"
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/aws/aws-sdk-go/service/ecr"
 	"github.com/aws/aws-sdk-go/service/ecr/ecriface"
+	"github.com/docker/docker/api"
+	"github.com/docker/docker/registry"
 )
 
 // Run runs the build process.
@@ -22,6 +26,7 @@ func Run(ecrClient ecriface.ECRAPI, runner CommandRunner, repository, buildID, v
 
 	image := repository + ":" + buildID + "-" + version
 
+	attemptToLoginToRegistriesInDockerFile(runner)
 	fmt.Fprintf(os.Stderr, "\n- Building docker image...\n\n")
 	fmt.Fprintf(os.Stderr, "$ docker build -t %s .\n\n", image)
 	runner.Run("docker", "build", "-t", image, ".")
@@ -45,4 +50,50 @@ func getCredentials(ecrClient ecriface.ECRAPI) (string, string) {
 	}
 	credentials := strings.Split(string(data), ":")
 	return credentials[0], credentials[1]
+}
+
+func attemptToLoginToRegistriesInDockerFile(runner CommandRunner) {
+	dockerfileFromLinePattern := regexp.MustCompile(`(?i)^[\s]*FROM[ \f\r\t\v]+(?P<image>[^ \f\r\t\v\n#]+)`)
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dockerfile, err := os.Open(cwd + "/Dockerfile")
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	defer dockerfile.Close()
+	scanner := bufio.NewScanner(dockerfile)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		matches := dockerfileFromLinePattern.FindStringSubmatch(line)
+		if matches != nil && matches[1] != api.NoBaseImageSpecifier {
+			imageRegistry := registry.IndexHostname
+			if strings.Count(matches[1], "/") > 1 {
+				imageRegistry = strings.Split(matches[1], "/")[0]
+			}
+			registryVarName := strings.ToUpper(
+				strings.NewReplacer(
+					".", "_",
+					":", "_",
+					"-", "_",
+				).Replace(imageRegistry),
+			)
+
+			cdflowDockerAuthPrefix := "CDFLOW2_DOCKER_AUTH_"
+			username := os.Getenv(cdflowDockerAuthPrefix + registryVarName + "_USERNAME")
+			password := os.Getenv(cdflowDockerAuthPrefix + registryVarName + "_PASSWORD")
+
+			if len(username) > 0 && len(password) > 0 {
+				fmt.Fprintf(os.Stderr, "- Found credentials for registry %s. Attempting to login...\n\n", imageRegistry)
+				runner.RunWithInput(password, "docker", "login", "-u", username, "--password-stdin", imageRegistry)
+			} else {
+				fmt.Fprintf(os.Stderr, "- Auth credentials not found for registry '%s'.\n", imageRegistry)
+				fmt.Fprintf(os.Stderr, "Access to this registry will be without auth.\n")
+				fmt.Fprintf(os.Stderr, "Set the appropriate environment variables if auth is required.\n\n")
+			}
+		}
+	}
 }
